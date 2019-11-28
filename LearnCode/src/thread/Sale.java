0658ddd167e1ee6fe92144fde3f5a25ca7e671c2
fromsep <fromsep@163.com>
2019-11-28 18:24:36 +0800

    多线程

---
+++ LearnCode/src/thread/Sale.java
@@ -0,0 +1,18 @@
+package thread;
+
+public class Sale implements Runnable {
+    private int productAmount = 100;
+
+    @Override
+    public synchronized void run() {
+        if(this.productAmount > 0) {
+            System.out.printf("[%s]售出第%d\t件商品\n", Thread.currentThread().getName(), this.productAmount);
+            this.productAmount--;
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ LearnCode/src/thread/TestOrder.java
@@ -0,0 +1,17 @@
+package thread;
+
+public class TestOrder {
+    public static void main(String[] args) {
+        Sale productSale = new Sale();
+
+        for (int i = 0; i < 200; i++) {
+            Thread thread1 = new Thread(productSale, "第1销售处");
+            Thread thread2 = new Thread(productSale, "第2销售处");
+            Thread thread3 = new Thread(productSale, "第3销售处");
+
+            thread1.start();
+            thread2.start();
+            thread3.start();
+        }
+    }
+}
